355959dbd1accdda5656ae00d1d84353140067c1
Surekha <surekhagidithuri@gmail.com>
2025-05-18 17:33:28 +0530

    variables intro

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-PERSON1=Karthik
-PERSON2=Amma
+PERSON1=$1
+PERSON2=$2
 #There should no space before and after '=' when initializing a variable
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hi Yuvan, I am doing good, what about you?"
