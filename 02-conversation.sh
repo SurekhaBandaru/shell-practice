185a3d4ff4fa8833cd3ae56dc7f802b6b7465584
Surekha <surekhagidithuri@gmail.com>
2025-05-18 16:56:36 +0530

    conversation file

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Kathik:: Hi Amma, How are you?"
+echo "Amma:: Hi Yuvan, I am doing good, what about you?"
+echo "Karthik:: I want to weat ice cream, can you give me?"
+echo "Amma:: No, you should not eat, I am not supposed to give you"
+echo "Karthik:: Oops"
