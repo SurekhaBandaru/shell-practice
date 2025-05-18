c496d7b129cce0a31f8e06d9371ce4ce4e4cdf9a
Surekha <surekhagidithuri@gmail.com>
2025-05-18 18:20:49 +0530

    variables intro

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,5 +2,6 @@
 MOVIES=("Court" "Chaava" "ThirtyFive" "Gudachaari")
 echo "first movie: ${MOVIES[0]}"
 echo "Last movie: ${MOVIES[3]}"
+echo "Fourth Movie: ${MOVIES[4]}"
 
 echo "All available movies: ${MOVIES[@]}"
