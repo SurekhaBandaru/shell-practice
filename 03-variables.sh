36f1294173c44e42665bbb9c686bc5f61f5dedd8
Surekha <surekhagidithuri@gmail.com>
2025-05-18 17:05:52 +0530

    variables intro

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+PERSON1 = Karthik
+PERSON2 = Amma
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi Yuvan, I am doing good, what about you?"
+echo "$PERSON1:: I want to weat ice cream, can you give me?"
+echo "$PERSON2:: No, you should not eat, I am not supposed to give you"
+echo "$PERSON1:: Oops"
