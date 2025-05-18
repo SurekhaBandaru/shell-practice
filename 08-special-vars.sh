39d2f66fa117afbe4b50c71edc77e1468343fc0e
Surekha <surekhagidithuri@gmail.com>
2025-05-18 21:53:21 +0530

    variables intro

---
+++ 08-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "Number of varables passed to script: $#"
+echo "Current script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of the user who is running the script: $HOME"
+echo "Which usee is running the script: $USER"
+echo "PID of current script: $$"
+echo "PID of last command running in background: $1"
