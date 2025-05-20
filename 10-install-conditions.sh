186e878587b90f7b124a636306f85827f3ce8cb1
Surekha <surekhagidithuri@gmail.com>
2025-05-20 23:37:48 +0530

    conditions

--- 10-install-conditions.sh
+++ 10-install-conditions.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if[ $USERID -ne 0 ]
     echo "ERROR:: Please run the script with root access"
+    exit 1 # give other than 0 till 127
 else
     echo "You are running with root access"
 fi
