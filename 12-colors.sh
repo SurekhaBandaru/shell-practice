eeaf3751cec674f62b48108890ac714379385e29
Surekha <surekhagidithuri@gmail.com>
2025-05-22 21:32:39 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -5,4 +5,4 @@ echo -e "Hello Colours"
 
 echo -e "\e[31m this is red color"
 #the nelow command will also be displayed in in red as we did not end the color command
-echo -e "continue on colors practice\[0m"
+echo -e "continue on colors practice\e[0m"
