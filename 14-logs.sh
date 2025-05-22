82900ffdc81c759bce6f4031b90416826772f3e5
Surekha <surekhagidithuri@gmail.com>
2025-05-22 23:28:58 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -12,15 +12,15 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=$LOG_DIR/$SCRIPT_NAME.log
 
 mkdir -p $LOG_DIR
-
-echo "Script started, executing at: $(date)" &>>$LOG_FILE 
+# | tee -a $LOG_FILE  -- to display on the screen as well as in log file
+echo "Script started, executing at: $(date)"  | tee -a $LOG_FILE 
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run the script with root access $N" &>>$LOG_FILE 
+    echo -e "$R ERROR:: Please run the script with root access $N" | tee -a $LOG_FILE
     exit 1 # give other than 0 till 127
 else
-    echo  -e "$G You are running with root access $N" &>>$LOG_FILE 
+    echo  -e "$G You are running with root access $N"  | tee -a $LOG_FILE
 fi
 
 VALIDATE () {
@@ -28,9 +28,9 @@ VALIDATE () {
 if [ $1 -eq 0 ] #if the installation is success
 #here $1 is first param (exit status $?), $2 is second param (software name)
     then 
-         echo -e "Installing $2 is ... $G SUCCESS $N" &>>$LOG_FILE 
+         echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE 
     else
-         echo -e "Installing $2 is a .... $R FAILURE $N" &>>$LOG_FILE 
+         echo -e "Installing $2 is a .... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
