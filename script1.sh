321ae80d4b3ebdbbafef8e3aa7158c9f8c974eae
Surekha <surekhagidithuri@gmail.com>
2025-06-01 22:26:14 +0530

    call script from another

---
+++ script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Greetings from script1"
+
+./script2.sh
+echo "PID of script1 : $$"
---
+++ script2.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "PID: $$"
+
+echo "Greetings from script2"
+
+echo "PID of script2 : $$"
