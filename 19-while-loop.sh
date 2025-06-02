8464b536c1ab0b82f1b15a3c9125fc4d6d27015c
Surekha <surekhagidithuri@gmail.com>
2025-06-02 18:10:16 +0530

    set cmd

--- 19-while-loop.sh
+++ 19-while-loop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#Ctlc+c to comment
+# a=0
 
-a=0
+# while [ $a -lt 10 ]
+# do
+#     echo $a
+#     a=`expr $a + 1`
+# done
 
-while [ $a -lt 10 ]
+#Here IFS is used to separate line by line, -r is to read special characters as well
+#IFS -> Internal field separator
+while IFS=read -r line 
 do
-    echo $a
-    a=`expr $a + 1`
-done
+echo $line 
+done <17-set.sh
