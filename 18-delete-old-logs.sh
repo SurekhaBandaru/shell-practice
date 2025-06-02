556ca997d598a1c67b01ff4d37971c4201470949
Surekha <surekhagidithuri@gmail.com>
2025-06-02 21:35:26 +0530

    delete old logs

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_DIR=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_DIR.log"
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" mtime +14)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at : $(date)" | tee -a $LOG_FILE
+
+#Check if user has root access
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run the script with sudo access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$Y You are running with root access $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is ..... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ...... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+while IFS= read -r filepath; do
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+done <<<$FILES_TO_DELETE
+
+echo "Script executed successfully"
--- 19-while-loop.sh
+++ 19-while-loop.sh
@@ -12,6 +12,7 @@
 
 #Here IFS is used to separate line by line, -r is to read special characters as well
 #IFS -> Internal field separator
+#the following loop will take input from 17-set.sh and reads line by line and prints everything starting from bin bash command
 while IFS= read -r line 
 do
     echo $line 
