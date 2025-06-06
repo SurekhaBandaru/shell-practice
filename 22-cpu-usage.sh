44bac3bd82429e81ee2b4d1508ab8a87d117df2e
Surekha <surekhagidithuri@gmail.com>
2025-06-07 00:07:37 +0530

    cpu usage

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-    cpu_idle=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }') # (top -b -n 1 | grep "Cpu(s)" | awk '{print $8}' | cut -d '.' -f 1)
+# (top -b -n 1 | grep "Cpu(s)" | awk '{print $8}' | cut -d '.' -f 1)
+    cpu_idle=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }') 
     cpu_usage=$((100 - cpu_idle))
     echo "CPU Usage: $cpu_usage%"
