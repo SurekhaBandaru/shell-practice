51eac755d8e383d1a734cae4ce7174f78c9a66a2
Surekha <surekhagidithuri@gmail.com>
2025-06-06 15:45:34 +0530

    archieve files

--- 20-backup.sh
+++ 20-backup.sh
@@ -29,14 +29,6 @@ fi
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at : $(date)" | tee -a $LOG_FILE
 
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 is ..... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ...... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
 
 USAGE() {
     echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
