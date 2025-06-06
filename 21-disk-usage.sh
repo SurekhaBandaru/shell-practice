e26fd37ea7e911619108537a379c4a1b63b1a2fd
Surekha <surekhagidithuri@gmail.com>
2025-06-06 23:56:38 +0530

    disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -12,7 +12,9 @@ while IFS= read line; do
     PARTITION=$(echo $line | awk '{print $7F}')              #get the Filesytem column where we can see at which location this memory used
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
     #hre + is to append to existing MSG, if not it is printing only last line
-        MSG+="High disk usage on $PARTITION: $USAGE \n"
+        #MSG+="High disk usage on $PARTITION: $USAGE \n" #\n i snew line in linux
+
+         MSG+="High disk usage on $PARTITION: $USAGE <br>" #here we need to split by new line in html so, user <br>
     fi
     
 done <<< $DISK_USAGE
