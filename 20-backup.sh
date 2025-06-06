5b0e27f9c1f4a9db1a09729365354bc5987174db
Surekha <surekhagidithuri@gmail.com>
2025-06-06 15:57:40 +0530

    archieve files

--- 20-backup.sh
+++ 20-backup.sh
@@ -19,7 +19,8 @@ LOG_FILE="$LOGS_FOLDER/backup.log"
 #Check if user has root access
 #| tee -a $LOG_FILE #commented out this log as facing some issues while making this script as a command
 check_root(){
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]
+then
     echo -e "$R ERROR:: Please run the script with sudo access $N" 
     exit 1
 else
@@ -27,46 +28,54 @@ else
 fi
 }
 
-mkdir -p $LOGS_FOLDER
 check_root
+mkdir -p $LOGS_FOLDER
+
 #echo "Script started executing at : $(date)" | tee -a $LOG_FILE
 
 
 USAGE() {
     echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
 }
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 2 ]
+then
     USAGE
 fi
 
 #check if directory exists -d <dir>
 #below is if dir not exist
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d $SOURCE_DIR ]
+then
     echo -e "$R source directory $SOURCE_DIR does not exist, please check $N"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]; then
+if [ ! -d $DEST_DIR ]
+then
     echo -e "$R destination directory $DEST_DIR does not exist, please check $N"
     exit 1
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ ! -z "$FILES" ]; then
+if [ ! -z "$FILES" ]
+then
     echo "Files to zip are: $FILES"
     TIME_STAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE="$DEST_DIR/app-logs-$TIME_STAMP.zip"
     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 
-    if [ -f $ZIP_FILE ]; then
+    if [ -f $ZIP_FILE ]
+    then
         echo -e "$Y Successfully created Zip file in $DEST_DIR $N"
-        while IFS= read -r filepath; do
+        while IFS= read -r filepath
+        do
             echo "Deleting file: $filepath" | tee -a $LOG_FILE
             rm -rf $filepath
         done <<< $FILES
-        echo -e "Log files older than $DAYS from source directory removed ... &G SUCCESS $N"
+        echo -e "Log files older than $DAYS from source directory removed ... $G SUCCESS $N"
     else
         echo -e "$R FAILURE $N.... Zip file is not created"
 
