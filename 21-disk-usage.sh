c663dd53e9939d20daf41369dd46066129a26415
Surekha <surekhagidithuri@gmail.com>
2025-06-06 22:49:29 +0530

    disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -9,4 +9,5 @@ while IFS= read -r line
 do
 USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1) #get the Use% column (without header) and without %
 PARTITION=$(echo $line | awk '{print $7F}') #get the Filesytem column where we can see at which location this memory used
+echo "$PARTITION: $USAGE"
 done <<< $DISK_USAGE
