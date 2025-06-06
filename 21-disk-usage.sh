30d3aff000beddd9036f0316abf48ef52b6a554f
Surekha <surekhagidithuri@gmail.com>
2025-06-06 22:48:35 +0530

    disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -5,7 +5,7 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #usually be 75 in real projects
 
 
-while IFS=read line
+while IFS= read -r line
 do
 USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1) #get the Use% column (without header) and without %
 PARTITION=$(echo $line | awk '{print $7F}') #get the Filesytem column where we can see at which location this memory used
